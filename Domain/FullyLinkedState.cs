b1b7ab59c7715f9f709bc163a905b287078fe633
niklasef <niklas.furberg@gmail.com>
2018-10-15 20:45:58 +0200

    moved half link state to separate file

--- Domain/FullyLinkedState.cs
+++ Domain/FullyLinkedState.cs
@@ -4,40 +4,6 @@ using System.Linq;
 
 namespace Domain
 {
-    internal class HalfLinkedState : ITileState
-    {
-        public IEnumerable<ushort> GetUnlinkedValue(DominoTile tile)
-        {
-            return new[] {
-                tile
-                .GetValues()
-                .First(x => !tile
-                    .LinkedTiles
-                    .First(t => t != null)
-                    .GetValues()
-                    .Any(y => x == y))};
-        }
-
-        public void Link(DominoTile tile, DominoTile otherTile)
-        {
-            if (tile == null)
-            {
-                throw new ArgumentNullException(nameof(tile));
-            }
-
-            if (otherTile == null)
-            {
-                throw new ArgumentNullException(nameof(otherTile));
-            }
-            if (!tile.MatchesUnlinkedValue(otherTile) || !otherTile.MatchesUnlinkedValue(tile))
-            {
-                throw new ApplicationException($"Illegal move, no matching unlinked values. Can't link: tile: '{tile}' with tile: '{otherTile}'");
-            }
-            tile.AddLinkedTile(otherTile);
-            otherTile.AddLinkedTile(tile);
-            tile.State = new FullyLinkedState();
-        }
-    }
 
     internal class FullyLinkedState : ITileState
     {
