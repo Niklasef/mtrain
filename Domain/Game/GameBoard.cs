4dc3b0c9a8209c55b833aa6617edb3ffa8410ad0
niklasef <niklas.furberg@gmail.com>
2019-05-04 22:21:03 +0200

    changed so player and train relation in gamebord is stored as separte property

--- Domain/Game/GameBoard.cs
+++ Domain/Game/GameBoard.cs
@@ -40,40 +40,39 @@ namespace Domain.Game
 
         public override string ToString()
         {
-            // var stringBuilder = new StringBuilder();
-            // stringBuilder.AppendLine($"Game State: '{GameState}'");
-            // stringBuilder.AppendLine($"Player with turn: '{Players.First(p => p.Key == PlayerIdWithTurn).Value}'");
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine($"Game State: '{GameState}'");
+            stringBuilder.AppendLine($"Player with turn: '{Players.First(p => p.Key == PlayerIdWithTurn).Value}'");
 
-            // var trainIndex = 1;
-            // PlayerTrains
-            //     .ToList()
-            //     .ForEach(pt =>
-            //     {
-            //         stringBuilder.AppendLine("");
-            //         stringBuilder.AppendLine($"Player: '{Players.First(p => p.Key == pt.Key).Value}'");
-            //         stringBuilder.AppendLine($"train Idx: '{trainIndex}' train id: '{pt.Value.Key}'");
-            //         stringBuilder.AppendLine($"{string.Join(",", pt.Value.Value.Reverse())}");
-            //         trainIndex++;
-            //     });
+            var trainIndex = 1;
+            PlayerTrains
+                .ToList()
+                .ForEach((i, pt) =>
+                {
+                    stringBuilder.AppendLine("");
+                    stringBuilder.AppendLine($"Player: '{Players.First(p => p.Key == pt.Key).Value}'");
+                    stringBuilder.AppendLine($"train Idx: '{trainIndex}' train id: '{pt.Value.Key}'");
+                    stringBuilder.AppendLine($"{string.Join(",", pt.Value.Value.Reverse())}");
+                    trainIndex++;
+                });
 
-            // stringBuilder.AppendLine("");
-            // stringBuilder.AppendLine($"Mexican train");
-            // var mexicanTrain = new Domain.Train.MexicanTrain(MexicanTrain.Key);
-            // MexicanTrain
-            //     .Value
-            //         .ToList()
-            //         .ForEach(t => mexicanTrain.ForceAddTile(t));
-            // stringBuilder.AppendLine($"Idx: '{trainIndex}'  id: '{mexicanTrain.Id}'");
-            // stringBuilder.AppendLine($"{mexicanTrain.ToString()}");
+            stringBuilder.AppendLine("");
+            stringBuilder.AppendLine($"Mexican train");
+            var mexicanTrain = new Domain.Train.MexicanTrain(MexicanTrain.Key);
+            MexicanTrain
+                .Value
+                    .ToList()
+                    .ForEach(t => mexicanTrain.ForceAddTile(t));
+            stringBuilder.AppendLine($"Idx: '{trainIndex}'  id: '{mexicanTrain.Id}'");
+            stringBuilder.AppendLine($"{mexicanTrain.ToString()}");
 
-            // stringBuilder.AppendLine("");
-            // stringBuilder.AppendLine("Hand");
-            // stringBuilder
-            //     .AppendLine(
-            //         string.Join(", ", Hand.Select((t, i) => $"{i + 1}: {t} {t.Id}")));
+            stringBuilder.AppendLine("");
+            stringBuilder.AppendLine("Hand");
+            stringBuilder
+                .AppendLine(
+                    string.Join(", ", Hand.Select((t, i) => $"{i + 1}: {t} {t.Id}")));
 
-            // return stringBuilder.ToString();
-            return "";
+            return stringBuilder.ToString();
         }
     }
 }
